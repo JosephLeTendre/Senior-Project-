6a8b8efa7c128791a465b7d425f40d8f571edd2f
Joseph LeTendre <jl620210@sju.edu>
2018-02-07 19:30:58 -0500

    Setup Commit
    Added about and home pages, as well as header and footer to make php coding easier.

---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>ciBlog</title>
+		<link rel="stylesheet" href="https://bootswatch.com/lux/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+	</head>
+	<body>
+	<nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="<?php echo base_url(); ?>">EZAPP</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li><a href="<?php echo base_url(); ?>about">About</a></li>
+          </ul>
+          </ul>
+        </div>
+      </div>
+    </nav>
