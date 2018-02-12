36ccd772dbb32f631a9adb6d406b6eb4e1b98856
Patrick Quirk <pq623516@sju.edu>
2018-02-11 21:13:16 -0500

    Small Changes to the templates
    Just taking out really generic stuff that came with the framework/navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,11 +1,11 @@
 <html>
   <head>
-    <title>ciBlog</title>
+    <title>EZ Club</title>
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
   </head>
   <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">EZClub</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
