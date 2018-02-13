fe3d6d502acbd7fdb6e5b7ebb69cb4642c9e294b
Joseph LeTendre <jl620210@sju.edu>
2018-02-13 15:00:12 -0500

    Updated PresHome
    Made two boxes for PresHome page.

--- application/views/pages/PresHome.php
+++ application/views/pages/PresHome.php
@@ -1,11 +1,18 @@
 <div id="container">
 	<h1>Welcome [INSERT CLUB'S NAME HERE]!</h1>
 
-	<div id="body">
-		<p>List of Club Members</p>
-		<code>[INSERT MEMBER'S NAMES HERE]</code>
+	<div id="body1" style = "float: left; width: 40%">
+		&nbsp
+		<h4>List of Club Members</h4>
+		<code>[INSERT MEMBER1'S NAME HERE]<br>
+			[INSERT MEMBER2'S NAME HERE]<br>
+			[INSERT MEMBER3'S NAME HERE]<br>
+			[INSERT MEMBER4'S NAME HERE]<br></code>
+	</div>
+	<div id="body2" style = "float: right; width: 45%">
+		&nbsp
 
-		<p>Executive Members</p>
+		<h4>Executive Members</h4>
 		<code>[INSERT PRES NAME HERE] <br>
 			  [INSERT VP NAME HERE] <br>
 			  [INSERT TREASURER NAME HERE] <br>
