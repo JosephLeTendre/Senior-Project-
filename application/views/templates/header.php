4da997b882aacc62e8483e69800e6e09ddffded4
Joseph LeTendre <jl620210@sju.edu>
2018-02-13 14:05:44 -0500

    Login Button Added
    Added Login to top right

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,7 +13,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="Home">Home<span class="sr-only">(current)</span></a>
       </li>
             <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>PresHome">PresHome</a>
@@ -37,10 +37,9 @@
             <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>testing">TESTING</a>
       </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>Login">Login</a>
+      </li>
+    </ul> 
   </div>
 </nav>
