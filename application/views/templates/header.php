4b0d371f2db2a259ee5e5cfe77b4ca67f0906583
Joseph LeTendre <jl620210@sju.edu>
2018-02-13 14:21:42 -0500

    EZClub Home Button
    Made it so when a user clicks EZCLUB it takes them home.
    
    Took out the Home button.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -61,16 +61,13 @@
   </head>
   <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">EZClub</a>
+  <a class="navbar-brand" href="Home">EZClub</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="Home">Home<span class="sr-only">(current)</span></a>
-      </li>
             <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>PresHome">PresHome</a>
       </li>
