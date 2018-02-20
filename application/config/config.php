37b6b315df9ec7cb565edd4cde28d76d2e071381
Patrick Quirk <pq623516@sju.edu>
2018-02-20 14:28:17 -0500

    Calendar
    changes to the calendar page

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Senior-Project-';
+$config['base_url'] = 'http://localhost:1234/Senior-Project-';
 
 /*
 |--------------------------------------------------------------------------
