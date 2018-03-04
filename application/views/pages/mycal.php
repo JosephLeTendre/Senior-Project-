fa34150241b7146d04366980cf17c080f8f54e66
Patrick Quirk <pq623516@sju.edu>
2018-03-03 20:55:27 -0500

    testing
    Testing again

--- application/views/pages/mycal.php
+++ application/views/pages/mycal.php
@@ -1,17 +1,23 @@
-<style type = "text/css">
-	.calendar {
-		font-family: Arial; font-size: 12px;
-	}
-	table.calendar{
-		margin: auto; border-collapse: collapse;
-	}
-	.calender .days td{
-		width: 80px; height:80px; padding: 4px;
-		border: 1px solid #999;
-		vertical-align: top;
-		background-color: #DEF;
-	}
-</style>
-<?php
-	echo $calendar;
-?>
+<html>
+  <head>
+  	<title>My Calendar </title>
+	<style type = "text/css">
+		.calendar {
+			font-family: Arial; font-size: 100px;
+		}
+		table.calendar{
+			margin: auto; border-collapse: collapse;
+		}
+		.calender .days td{
+			width: 80px; height:80px; padding: 4px;
+			border: 1px solid #999;
+			vertical-align: top;
+			background-color: #DEF;
+		}
+	</style>
+</head>
+<body>
+
+<?php echo $calendar; ?>
+</body>
+</html>
