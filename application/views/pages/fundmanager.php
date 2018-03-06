e7ea80a57741fd8ba133e0b0368c97e32aa143b7
Patrick Quirk <pq623516@sju.edu>
2018-03-06 13:49:55 -0500

    new format of fund manager
    Displays a clubs current/total budget from the database

--- application/views/pages/fundmanager.php
+++ application/views/pages/fundmanager.php
@@ -4,7 +4,23 @@
 	<div id="body1" style = "text-align: center;">
 		&nbsp
 		<h4>Budget:</h4>
-			<code>[INSERT CURRENT BUDGET HERE] / [INSTERT TOTAL BUDGET HERE]<br></code>
+			<code>
+				<?php 
+					$this->load->library('table');
+					$this->table->set_heading('Current Budget');
+					$style = array('table_open'  => '<table class="table table-bordered table-hover">');
+	    			$this->table->set_template($style);
+		 			$current = $this->db->query("SELECT CurrentBudget FROM club;");
+		 			echo $this->table->generate($current);
+				?>
+				<?php
+					$this->load->library('table');
+					$this->table->set_heading('Total Budget');
+					$style = array('table_open'  => '<table class="table table-bordered table-hover">');
+	    			$this->table->set_template($style);
+					$total = $this->db->query("SELECT InitialBudget FROM club;");
+					echo $this->table->generate($total); 
+				?><br></code>
 	</div>
 	<div id="body1" style = "text-align: center; width: 80%">
 		&nbsp
