660c4b1b95ebe6f7512b7c5a1930470f4136ae89
Angelynn Rodriguez <ar625308@sju.edu>
2018-03-07 16:46:38 -0500

    Login Changes

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database','table');
+$autoload['libraries'] = array('database','table','form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -134,4 +134,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('Mycal_model','DisplayMembers');
+$autoload['model'] = array('Mycal_model','DisplayMembers','login_database');
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-<script type="text/javascript" id="lbscript1028374" src="https://www.benchmarkemail.com/code/lbformnew.js?mFcQnoBFKMRhPxCcVP%252Fft6jgVtVj7bhtV4NQh9oM4nKgMY2OfLKlNzciHg%252FVbFy1"></script><noscript>Please enable JavaScript <br /><div align="center" style="padding-top:5px;font-family:Arial,Helvetica,sans-serif;font-size:10px;color:#999999;"><a href="//www.benchmarkemail.com" target="_new" style="text-decoration:none;font-family:Arial,Helvetica,sans-serif;font-size:10px;color:#999999;">Email Marketing Services</a> by Benchmark</div></noscript>
+
 
 </head>
 </html>
