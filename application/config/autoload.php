808d52869441e4713f735db3500a7467f8379c2b
Patrick Quirk <pq623516@sju.edu>
2018-03-15 13:03:03 -0400

    Just trying to make this work
    I'll change it back I promise

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -134,4 +134,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('Mycal_model','DisplayMembers','login_database');
+$autoload['model'] = array('Mycal_model','DisplayMembers');
