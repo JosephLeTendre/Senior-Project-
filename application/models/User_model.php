b3468123c61e8ccbc6009d026611ea1a222c1993
Angelynn Rodriguez <ar625308@sju.edu>
2018-03-21 15:12:22 -0400

    Connecting to DB and new Table

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,7 +7,7 @@
 				'email' => $this->input->post('email'),
                 'username' => $this->input->post('username'),
                 'password' => $enc_password,
-                'zipcode' => $this->input->post('zipcode')
+                
 			);
 			// Insert user
 			return $this->db->insert('users', $data);
