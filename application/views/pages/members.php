c4b3c5fccba8541d9b1f156c6b2bb2a313d0f4b9
Patrick Quirk <pq623516@sju.edu>
2018-03-26 10:59:50 -0400

    members table changed
    id not shown and table header changed

--- application/views/pages/members.php
+++ application/views/pages/members.php
@@ -5,11 +5,11 @@
 <?php
 
 		$this->load->library('table');
-		$this->table->set_heading('UserID','FirstName','Email');
+		$this->table->set_heading('Name','Email');
 
 		$style = array('table_open'  => '<table class="table table-bordered table-hover">');
 	    $this->table->set_template($style);
-	    $members = $this->db->query("SELECT id, name, email FROM users;");
+	    $members = $this->db->query("SELECT name, email FROM users;");
 	    echo $this->table->generate($members);
 
 
