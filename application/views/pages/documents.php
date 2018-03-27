f606f75eb837ad46ebcf3fa51d140dc629ea1130
Patrick Quirk <pq623516@sju.edu>
2018-03-27 14:06:46 -0400

    iframe
    test

--- application/views/pages/documents.php
+++ application/views/pages/documents.php
@@ -1,5 +1,5 @@
 
 <br>
 <div id="body1" style = "font-size: 20">
-<iframe src="https://forms.sju.edu/sla/view.php?id=22" title="iframe example 1" seamless="seamless" scrolling="no" style="width:100%; height: 100%; float: left;">
+<iframe src="https://forms.sju.edu/sla/view.php?id=22" title="iframe example 1" seamless="seamless" scrolling="no" style="width:100%; height: 100%; float: left;"></iframe>
   
