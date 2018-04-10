27eda9dd4128bff904e117f856e01a00316cbcb9
Joseph LeTendre <jl620210@sju.edu>
2018-04-10 14:51:58 -0400

    Changing Fonts
    Changes fonts to be more readable.

--- application/views/pages/fundmanager.php
+++ application/views/pages/fundmanager.php
@@ -8,7 +8,7 @@
 				<?php 
 					$this->load->library('table');
 					$this->table->set_heading('Current Budget');
-					$style = array('table_open'  => '<table class="table table-bordered table-hover">');
+					$style = array('table_open'  => '<table class="table table-bordered table-hover text-primary">');
 	    			$this->table->set_template($style);
 		 			$current = $this->db->query("SELECT CurrentBudget FROM club;");
 		 			echo $this->table->generate($current);
@@ -16,7 +16,7 @@
 				<?php
 					$this->load->library('table');
 					$this->table->set_heading('Total Budget');
-					$style = array('table_open'  => '<table class="table table-bordered table-hover">');
+					$style = array('table_open'  => '<table class="table table-bordered table-hover text-primary">');
 	    			$this->table->set_template($style);
 					$total = $this->db->query("SELECT InitialBudget FROM club;");
 					echo $this->table->generate($total); 
@@ -30,7 +30,7 @@
 			<code><?php
 					$this->load->library('table');
 					$this->table->set_heading('Event Name','Date','Cost of Event');
-					$style = array('table_open'  => '<table class="table table-bordered table-hover">');
+					$style = array('table_open'  => '<table class="table table-bordered table-hover text-primary">');
 	    			$this->table->set_template($style);
 					$transactions = $this->db->query("SELECT EventName, DATE, TotalCost FROM events;");
 					echo $this->table->generate($transactions); 
@@ -42,23 +42,22 @@
 		<br>
 		<h4> Add New Budget Item </h4>
 		<?php echo validation_errors(); ?>
-		<?php echo form_open('funds/addItem');?>
-			<div class="form-group">
-				<label>Name</label>
+		<?php echo form_open('Funds/addItem');?>
+			<div class="form-group text-primary">
+				<label>Name of Event:</label>
 				<input type="text" name="name" class="form-control" style="width: 25%">
 			</div>
-			<div class="form-group">
-				<label>Date (YYYY-MM-DD)</label>
+			<div class="form-group text-primary">
+				<label>Date of Event(YYYY-MM-DD):</label>
 				<input type="text" name="date" class="form-control" style="width: 25%">
 			</div>
-			<div class="form-group">
-				<label>Cost </label>
+			<div class="form-group text-primary">
+				<label>Cost of Event:</label>
 				<input type="text" name="cost" class="form-control" style="width: 25%">
 			</div>
 			<input type="hidden" name="slug" value='<?php echo $post['slug']; ?>'>
 			<button class="btn btn-primary" type="submit"> Submit </button>
 
 	</div>
-	<?php echo form_close();?>
 
 </div>
--- application/views/pages/members.php
+++ application/views/pages/members.php
@@ -2,24 +2,20 @@
 <html>
 <body>
 
-<?php
 
+<?php
 		$this->load->library('table');
 		$this->table->set_heading('Name','Email');
 
-		$style = array('table_open'  => '<table class="table table-bordered table-hover">');
+		$style = array('table_open'  => '<table class="table table-bordered table-hover text-primary">');
 	    $this->table->set_template($style);
 	    $members = $this->db->query("SELECT name, email FROM users;");
 	    echo $this->table->generate($members);
-
 
 	// $this->load->library('table');
 	// $query = $this->db->query("SELECT * FROM login");
 	// echo $this->table->generate($query);
 ?>
-
-
-
 
 
 
