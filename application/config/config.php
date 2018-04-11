f3e0b48e03cbefdf5bf657871f721303ab52e7ac
Angelynn Rodriguez <ar625308@sju.edu>
2018-04-11 16:40:30 -0400

    testing some stuff so changing to local host rq.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://35.226.62.243/Senior-Project-';
+$config['base_url'] = 'http://localhost/Senior-Project-';
 
 
 /*
